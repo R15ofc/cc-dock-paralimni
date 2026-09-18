0a7cfd9b462373d3b62e31232766231d8a55b2c3
R15ofc <R15ofc@users.noreply.github.com>
2026-09-18 22:12:59 +0300

    Pin RoadRover installer to 2.5.0

--- car-installer.lua
+++ car-installer.lua
@@ -1,4 +1,4 @@
-local RELEASE = "935902bf1604b9edd75a11e45089b3e22eefd57c"
+local RELEASE = "a61cd3c932753ce9cf2b70d845d4549bd77d530c"
 local RELEASE_BASE = "https://raw.githubusercontent.com/R15ofc/cc-dock-paralimni/" .. RELEASE .. "/"
 local TARGET = "startup.lua"
 local VEHICLE_INFO = "VehicleInfo.json"
