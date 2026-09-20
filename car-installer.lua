e87551d81e386ea5dee33da3016e1addb00b7cfd
R15 <r15@R15s-MacBook-Air.local>
2026-09-20 19:45:21 +0300

    Point installer at RoadRover 2.9.3

--- car-installer.lua
+++ car-installer.lua
@@ -1,4 +1,4 @@
-local RELEASE = "30f1441c99103eefd5cd91ad3aeb247458d45e67"
+local RELEASE = "1fedfd1fa20aa51c7253332053d161fedba68d5c"
 local RELEASE_BASE = "https://raw.githubusercontent.com/R15ofc/cc-dock-paralimni/" .. RELEASE .. "/"
 local TARGET = "startup.lua"
 local VEHICLE_INFO = "VehicleInfo.json"
