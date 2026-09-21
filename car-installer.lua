e4bb1878474080844c6c6af67134288fa089f8f4
R15 <r15@R15s-MacBook-Air.local>
2026-09-21 12:23:48 +0300

    Pin installer to RROS 2.12.3

--- car-installer.lua
+++ car-installer.lua
@@ -1,4 +1,4 @@
-local RELEASE = "b7e3804ae69a64d069a9be3c06ed8e208457e88d"
+local RELEASE = "acce195981213597cec999870a1298fdacb738dc"
 local RELEASE_BASE = "https://raw.githubusercontent.com/R15ofc/cc-dock-paralimni/" .. RELEASE .. "/"
 local TARGET = "startup.lua"
 local VEHICLE_INFO = "VehicleInfo.json"
